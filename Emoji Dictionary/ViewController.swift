51fdc9815032a717bfb3445aabc8b5d7d6e00f57
Prasanna Venkatesh <prasanvenkyr@gmail.com>
2017-09-19 08:25:52 +0530

    Added TableView and 2nd View Controller

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -8,11 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var emoji = ["😊", "🦊", "⚽️", "🖥", "⏰", "😍", "😈", "✌🏻", "🤙🏻", "🐬"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emoji.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
+        let cell = UITableViewCell ()
+        cell.textLabel?.text = emoji[indexPath.row]
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
